f5890387ac92056513426a4e1e5e6aa18babb36f
Muhammad Maauz Mansoor <muhammadmaauz2018@gmail.com>
2024-10-16 22:34:54 +0500

    Add files via upload

---
+++ Lab#5 (Tasks)/Activity#2/Activity#2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace program
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            TestCatch2();
+        }
+
+        static void TestCatch2()
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(@"C:\Users\Online Computer\OneDrive\Desktop\233599,VP,LAB#5\Activity#2\test.txt")) 
+                {
+                    sw.WriteLine("hello");
+                    Console.WriteLine("Wrote to file: C:\\Users\\Online Computer\\OneDrive\\Desktop\\233599,VP,LAB#5\\Activity#2\\test.txt");
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Done");
+            }
+        }
+    }
+}
