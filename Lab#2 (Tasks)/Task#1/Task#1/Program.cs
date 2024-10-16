70be98335f7c762b6bf4cbbf62a58c238e269cbe
Muhammad Maauz Mansoor <muhammadmaauz2018@gmail.com>
2024-10-16 22:21:20 +0500

    Add files via upload

---
+++ Lab#2 (Tasks)/Task#1/Task#1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: " + "Maauz Mansoor");
+            Console.WriteLine("Registration No: " + "18977453426");
+            Console.WriteLine("Address: " + "Multan");
+        }
+    }
+}
---
+++ Lab#2 (Tasks)/Task#2 (Part-B)/Task#2 (Part-B)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace argumentpassing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine(args[0]);
+                Console.WriteLine(args[1]);
+            }
+        }
+    }
+}
