394b847bf2e703fabbd32cab5a34f84ae508db60
Muhammad Maauz Mansoor <muhammadmaauz2018@gmail.com>
2024-10-16 22:28:06 +0500

    Add files via upload

---
+++ Lab#3 (Tasks)/Task#7/Task#7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter first number:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter third number:");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+
+        int smallest = Math.Min(num1, Math.Min(num2, num3));
+        Console.WriteLine($"The smallest number is: {smallest}");
+    }
+}
