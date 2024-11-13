d7483deec9a864e7d48c096ae5a49e1f8431d9a7
Muhammad Maauz Mansoor <muhammadmaauz2018@gmail.com>
2024-11-14 00:14:16 +0500

    Add files via upload

---
+++ Lab#6 (Tasks)/Exercise#9/Exercise#9/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Exercise_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblCharactersLeft = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.MaxLength = 160;
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(776, 288);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // lblCharactersLeft
+            // 
+            this.lblCharactersLeft.AutoSize = true;
+            this.lblCharactersLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCharactersLeft.Location = new System.Drawing.Point(239, 342);
+            this.lblCharactersLeft.Name = "lblCharactersLeft";
+            this.lblCharactersLeft.Size = new System.Drawing.Size(270, 32);
+            this.lblCharactersLeft.TabIndex = 1;
+            this.lblCharactersLeft.Text = "Characters Left: 160";
+            this.lblCharactersLeft.Click += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCharactersLeft);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Character Limit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblCharactersLeft;
+    }
+}
+
---
+++ Lab#6 (Tasks)/Exercise#9/Exercise#9/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_9
+{
+    public partial class Form1 : Form
+    {
+        private const int MaxCharacters = 160;
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateCharacterCount();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+       
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Text changed event triggered");
+            UpdateCharacterCount();
+        }
+
+        private void UpdateCharacterCount()
+        {
+            int charactersLeft = MaxCharacters - txtInput.Text.Length;
+            lblCharactersLeft.Text = $"Characters Left: {charactersLeft}";
+            Console.WriteLine($"Characters Left: {charactersLeft}");
+        }
+
+    }
+}
