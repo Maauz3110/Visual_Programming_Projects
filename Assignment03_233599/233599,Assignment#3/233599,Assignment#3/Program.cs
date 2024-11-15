4d035f5ebd59355e176d123dff9da7ad2770aa7f
Muhammad Maauz Mansoor <muhammadmaauz2018@gmail.com>
2024-11-15 17:29:21 +0500

    Add files via upload

---
+++ Assignment03_233599/233599,Assignment#3/233599,Assignment#3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Xml;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CreateXML();
+        ReadXML();
+    }
+
+    static void CreateXML()
+    {
+        XmlWriterSettings settings = new XmlWriterSettings
+        {
+            Indent = true,
+            IndentChars = "\t"
+        };
+
+        using (XmlWriter writer = XmlWriter.Create("GPS.xml", settings))
+        {
+            writer.WriteStartDocument();
+            writer.WriteStartElement("GPS_Log");
+
+            writer.WriteStartElement("Position");
+            writer.WriteAttributeString("DateTime", DateTime.Now.ToString());
+            writer.WriteElementString("x", "65.8973342");
+            writer.WriteElementString("y", "72.3452346");
+
+            writer.WriteStartElement("SatteliteInfo");
+            writer.WriteElementString("Speed", "40");
+            writer.WriteElementString("NoSatt", "7");
+            writer.WriteEndElement();
+
+            writer.WriteEndElement();
+
+            writer.WriteStartElement("Image");
+            writer.WriteAttributeString("Resolution", "1024x800");
+            writer.WriteElementString("Path", @"\images\1.jpg");
+            writer.WriteEndElement();
+
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+        }
+
+        Console.WriteLine("XML file 'GPS.xml' created successfully.");
+    }
+
+    static void ReadXML()
+    {
+        using (XmlReader reader = XmlReader.Create("GPS.xml"))
+        {
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    Console.WriteLine($"Element: {reader.Name}");
+                }
+                else if (reader.NodeType == XmlNodeType.Text)
+                {
+                    Console.WriteLine($"Value: {reader.Value}");
+                }
+                else if (reader.NodeType == XmlNodeType.EndElement)
+                {
+                    Console.WriteLine($"End Element: {reader.Name}");
+                }
+                else if (reader.NodeType == XmlNodeType.Attribute)
+                {
+                    Console.WriteLine($"Attribute: {reader.Name} = {reader.Value}");
+                }
+            }
+        }
+    }
+}
