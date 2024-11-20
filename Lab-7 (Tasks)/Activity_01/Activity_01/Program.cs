5ca6e42fb9db0668691b15998951591906ac9831
Muhammad Maauz Mansoor <muhammadmaauz2018@gmail.com>
2024-11-20 15:00:37 +0500

    Add files via upload

---
+++ Lab-7 (Tasks)/Activity_01/Activity_01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.OleDb;
+
+class Program
+{
+    static void Main()
+    {
+        // Replace 'C:\\Users\\Online Computer\\Downloads\\Northwind.mdb' with your actual database file path
+        string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Online Computer\\Downloads\\Northwind.mdb";
+        string queryString = "SELECT ProductID, UnitPrice, ProductName FROM Products WHERE UnitPrice > @pricePoint ORDER BY UnitPrice DESC;";
+        int paramValue = 5;
+
+        using (OleDbConnection connection = new OleDbConnection(connectionString))
+        {
+            OleDbCommand command = new OleDbCommand(queryString, connection);
+            command.Parameters.AddWithValue("@pricePoint", paramValue);
+
+            try
+            {
+                connection.Open();
+                using (OleDbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("\t{0}\t{1}\t{2}", reader[0], reader[1], reader[2]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
